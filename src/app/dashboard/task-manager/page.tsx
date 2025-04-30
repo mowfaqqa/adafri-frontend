494feb5c3ed6a9b08debf3dfca09c570a8334713
muwaffaq <itzmuwaffaq@gmail.com>
2025-04-30 12:25:12 +0100

    deleted file

--- src/app/dashboard/task-manager/page.tsx
+++ src/app/dashboard/task-manager/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 import ProjectDashboard from "@/components/TaskManager/ProjectDashboard";
-import TaskManagerOverview from "@/components/TaskManager/TaskManagerOverview";
 import { ProjectProvider } from "@/lib/context/task-manager/ProjectContext";
-import { TaskManagerProvider } from "@/lib/context/TaskmanagerContext";
 
 const TaskManager = () => {
   return (

b7ffa0bf859ff9c4543cd0c70b66f3893b274344
ExtraordinaryPD <anuoluwapochrist7@gmail.com>
2025-05-06 11:12:47 +0100

    landing page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+//  View Dashboard
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="max-w-xl mx-auto flex justify-center items-center">
+        <Link href="/dashboard/UserTestDashboard" passHref>
+          <Button className="bg-emerald-500 text-white">View Dashboard</Button>
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // "use client";
 // import React, { useState, useEffect } from "react";
 // import Image from "next/image";
@@ -892,19 +939,3 @@
 
 
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
-
-//  View Dashboard
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="max-w-xl mx-auto flex justify-center items-center">
-        <Link href="/dashboard/UserTestDashboard" passHref>
-          <Button className="bg-emerald-500 text-white">View Dashboard</Button>
-        </Link>
-      </main>
-    </div>
-  );
-}
